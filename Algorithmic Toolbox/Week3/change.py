af0bf044c5c3e6919556f003299586535f5449bb
Siddharth <think.siddharth@gmail.com>
2016-07-05 21:06:41 +0530

    new code

---
+++ Algorithmic Toolbox/Week3/change.py
@@ -0,0 +1,20 @@
+# Uses python2
+
+
+def get_change(n):
+    change = 0
+    while (n != 0):
+        if (n > 10):
+            n -= 10
+            change += 1
+        elif (n > 3):
+            n -= 10
+            change += 1
+        else:
+            n -= 1
+            change += 1
+    return change
+
+
+n = int(raw_input())
+print get_change(n)
